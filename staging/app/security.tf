63cf1bef4d9a44182b564083605a75a5e5dbf8be
Truman Shuck <truman@hatchloyalty.com>
2020-02-12 01:57:18 -0600

    Attach ssl certificate to lb and allow it to listen on 443

--- staging/app/security.tf
+++ staging/app/security.tf
@@ -5,8 +5,8 @@ resource "aws_security_group" "lb-staging" {
 
   ingress {
     protocol    = "tcp"
-    from_port   = var.app_port
-    to_port     = var.app_port
+    from_port   = 443
+    to_port     = 443
     cidr_blocks = ["0.0.0.0/0"]
   }
 
